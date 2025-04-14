cbfb02f9fd761015545804c4d495387342869b36
Raoul van Hezel <r.vanhezel@gmail.com>
2025-04-14 15:27:23 +0200

    try catch block in main

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ from src.utilities.logger import Logger
 from src.configuration import Configuration
 from dotenv import load_dotenv
 from src.execution_orchestrator import ExecutionOrchestrator
+import logging
 
 
 if __name__ == "__main__":
     load_dotenv()
 
-    Logger()
-    cfg = Configuration('run.cfg')
+    try:
 
-    trading_system = ExecutionOrchestrator(cfg)
-    trading_system.start()
+        Logger()
+        cfg = Configuration('run.cfg')
+
+        trading_system = ExecutionOrchestrator(cfg)
+        trading_system.start()
+
+    except Exception as e:
+        logging.error(f"Engine initialization failed: {e}")
